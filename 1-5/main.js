9be8ac1abbb856c1e7aae42cb9bc414df970bf09
Ryuya Hamada <m84.hamada.ryuya@gmail.com>
2021-03-08 21:43:15 +0900

    関数の課題提出

---
+++ 1-5/main.js
@@ -0,0 +1,8 @@
+function createJuice(fruits){
+  console.log(fruits + "を受け取りました。ジュースにして返します");
+  console.log(fruits + "ジュースが届きました");
+}
+
+
+let orangeJuice = createJuice("みかん");
+let grapeJuice = createJuice("ぶどう");
