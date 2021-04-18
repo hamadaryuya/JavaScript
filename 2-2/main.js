7e2aba406d125e023d60a82d9e19846f58c6dbd1
Ryuya Hamada <m84.hamada.ryuya@gmail.com>
2021-04-19 08:57:58 +0900

    要素の取得2の課題

---
+++ 2-2/main.js
@@ -0,0 +1,11 @@
+
+
+function changeBtn() {
+  let change = document.getElementById('p1');
+
+  if(change.style.display=="block"){
+    change.style.display = "none";
+  }else{
+    change.style.display = "block";
+  }
+}
